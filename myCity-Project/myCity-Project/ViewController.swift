e4cf86e1fd78f49933edd2afe22d69d7c2585787
Henry Calderon <theceintiant@gmail.com>
2020-06-15 17:42:42 -0700

    Added mobile side

---
+++ myCity-Project/myCity-Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myCity-Project
+//
+//  Created by Henry Calderon on 6/15/20.
+//  Copyright © 2020 Henry Calderon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
