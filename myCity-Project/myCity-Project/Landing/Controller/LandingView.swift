1c36c044a0f3f12c6c5c2457ae594f763846289c
Henry Calderon <theceintiant@gmail.com>
2020-06-15 22:09:18 -0700

    Working on the views themselves

---
+++ myCity-Project/myCity-Project/Landing/Controller/LandingView.swift
@@ -0,0 +1,22 @@
+//
+//  LandingView.swift
+//  myCity-Project
+//
+//  Created by Henry Calderon on 6/15/20.
+//  Copyright © 2020 Henry Calderon. All rights reserved.
+//
+
+import UIKit
+
+class LandingView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+
+}
---
+++ myCity-Project/myCity-Project/Landing/Controller/LandingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LandingViewController.swift
+//  myCity-Project
+//
+//  Created by Henry Calderon on 6/15/20.
+//  Copyright © 2020 Henry Calderon. All rights reserved.
+//
+
+import UIKit
+
+class LandingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
