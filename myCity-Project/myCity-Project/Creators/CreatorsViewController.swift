63d81f1bfe05e818c6bc3326504056f43e15ecce
Henry Calderon <theceintiant@gmail.com>
2020-06-29 23:13:28 -0700

    Redo of logo for icon purposes

--- myCity-Project/myCity-Project/Creators/CreatorsViewController.swift
+++ myCity-Project/myCity-Project/Creators/CreatorsViewController.swift
@@ -16,15 +16,57 @@ class CreatorsViewController: UIViewController {
         HeaderSection(),
         ProfileSection()
     ]
+    
+    lazy var collectionViewLayout: UICollectionViewLayout = {
+        var section = self.sections
+        let layout = UICollectionViewCompositionalLayout{
+            (sectionIndex, environment) -> NSCollectionLayoutSection? in
+            return self.sections[sectionIndex].layoutSection()
+        }
+        return layout
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .blue
+        self.view.backgroundColor = .white
         setPage()
     }
     
     func setPage(){
-        
+        collectView = UICollectionView(frame: view.bounds, collectionViewLayout: collectionViewLayout)
+        collectView.delegate = self
+        collectView.dataSource = self
+        collectView.backgroundColor = .white
+        collectView.register(UINib(nibName: "HeaderCell", bundle: .main), forCellWithReuseIdentifier: HeaderCell.identifier)
+        collectView.register(UINib(nibName: "ProfileCell", bundle: .main), forCellWithReuseIdentifier: ProfileCell.identifier)
+        self.view.addSubview(collectView)
+        collectView.reloadData()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        collectView.reloadData()
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        collectView.reloadData()
     }
 
+}
+
+extension CreatorsViewController: UICollectionViewDelegate, UICollectionViewDataSource{
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        sections.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        sections[section].numberOfItems
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        sections[indexPath.section].configureCell(collectionView: collectionView, indexPath: indexPath)
+    }
+    
+    
 }
