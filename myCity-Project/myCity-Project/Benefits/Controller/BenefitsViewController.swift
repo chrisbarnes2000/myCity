e157d25097ab88e1383d8ce045d19f60e6fc9cf6
Henry Calderon <theceintiant@gmail.com>
2020-06-29 23:38:43 -0700

    Mobile coming together, but lack of cohesion in some things. Edits coming.

---
+++ myCity-Project/myCity-Project/Benefits/Controller/BenefitsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BenefitsViewController.swift
+//  myCity-Project
+//
+//  Created by Henry Calderon on 6/29/20.
+//  Copyright © 2020 Henry Calderon. All rights reserved.
+//
+
+import UIKit
+
+class BenefitsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
