ecd57202e2fbad1c14f4ddd4231eeebfbefa41f9
Henry Calderon <theceintiant@gmail.com>
2020-07-02 10:35:49 -0700

    Added tab bar resources. Also added Pandemic view

--- myCity-Project/myCity-Project/Tab-Bar/TabBarController.swift
+++ myCity-Project/myCity-Project/Tab-Bar/TabBarController.swift
@@ -25,17 +25,23 @@ class TabBarController: UITabBarController, UITabBarControllerDelegate {
         let home = HomeViewController()
         home.title = "Home"
         let navHome = UINavigationController(rootViewController: home)
-        home.tabBarItem = UITabBarItem(tabBarSystemItem: .featured, tag: 0)
+        home.tabBarItem = UITabBarItem(title: "Home", image: UIImage(named: "house"), tag: 0)
         
         let resources = ResourceViewController()
         resources.title = "Resources"
         let navRes = UINavigationController(rootViewController: resources)
-        resources.tabBarItem = UITabBarItem(tabBarSystemItem: .bookmarks, tag: 1)
+        resources.tabBarItem = UITabBarItem(title: "Resources", image: UIImage(named: "wrench"), tag: 1)
+        
+        let pandemic = PandemicViewController()
+        pandemic.title = "Pandemic"
+        let navPandem = UINavigationController(rootViewController: pandemic)
+        pandemic.tabBarItem = UITabBarItem(title: "Pandemic", image: UIImage(named: "coronavirus"), tag: 2)
         
         let creators = CreatorsViewController()
         creators.title = "Creators"
         let navCreate = UINavigationController(rootViewController: creators)
-        creators.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 2)
+        creators.tabBarItem = UITabBarItem(title: "Creators", image: UIImage(named: "creators"), tag: 3)
+//        creators.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 2)
    
 //        marketplace.tabBarItem = UITabBarItem(title: "Market", image: UIImage(named:"marketplace"), tag: 0)
         
@@ -44,10 +50,23 @@ class TabBarController: UITabBarController, UITabBarControllerDelegate {
         //        let navProf = UINavigationController(rootViewController: profileVC)
         //        profileVC.tabBarItem = UITabBarItem(title: "Profile", image: UIImage(named:"user"), tag: 1)
         
-        viewControllers = [navHome, navRes, navCreate]
+        viewControllers = [navHome, navRes, navPandem, navCreate]
     }
     
     func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
         print("Selected a new view controller")
     }
 }
+
+//MARK: Attributes
+//Home
+//Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+
+//Resources
+//Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+
+//Creators
+//Icons made by <a href="https://www.flaticon.com/authors/eucalyp" title="Eucalyp">Eucalyp</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+
+//Pandemic
+//Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
